df2e46d818e83397464450005b180916654e44e0
Thiago Aksenen <aksenen1.thiago@gmail.com>
2025-09-09 22:21:59 -0300

    Agora os seeders funcionam

--- database/migrations/2025_09_10_004032_create_users_table.php
+++ database/migrations/2025_09_10_004032_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration {
     /**
@@ -12,7 +13,7 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id()->autoincrement();
-            $table->string('name', 50)->nullable(); // ou keep username
+            $table->string('username', 50)->nullable();
             $table->string('email', 50)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 200)->nullable();
