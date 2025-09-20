9d7d1c89078a01b78046797fb478ef46fc2f95c3
ShauSantos <shauaneeliza@gmail.com>
2025-09-20 20:53:17 -0300

    teste branch

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     }
     public function login()
     {
-        echo 'login';
+        echo 'login novo';
     }
     public function register(Request $request)
     {
