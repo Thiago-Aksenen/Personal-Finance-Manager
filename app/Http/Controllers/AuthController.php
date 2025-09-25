4d116878660dc26423e3eb689c2cc94a6f101fe8
ShauSantos <shauaneeliza@gmail.com>
2025-09-25 12:41:23 -0300

    meu commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     }
     public function login()
     {
-        echo 'login novo';
+        return view('login');
     }
     public function register(Request $request)
     {
@@ -49,6 +49,38 @@ class AuthController extends Controller
         $password = $request->input('password');
 
         echo 'Usuário: ' . $username . '<br>';
+        echo 'Email: ' . $email . '<br>';
+        echo 'Senha: ' . $password;
+    }
+
+    public function enter(Request $request)
+    {
+        $request->validate(
+            [
+                'username' => 'required|min:3|max:20',
+                'email' => 'required|email',
+                'password' => 'required|min:5|max:10',
+            ],
+            [
+                'email.required' => 'O campo de email é obrigatório!',
+                'email.email' => 'O valor digitado deve ser um email!',
+
+                'password.required' => 'O campo de senha é obrigatório!',
+                'password.min' => 'O campo de senha deve ter no mínimo 3 caracteres!',
+                'password.max' => 'O campo de senha deve ter no máximo 20 caracteres!',
+            ],
+        );
+
+        try {
+            DB::connection()->getPdo();
+            echo 'Connection is OK <br>';
+        } catch (\PDOException $e) {
+            echo 'A conexão falhou: ' . $e->getMessage();
+        }
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
         echo 'Email: ' . $email . '<br>';
         echo 'Senha: ' . $password;
     }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\AuthController;
 Route::get('/homepage', [AuthController::class, 'homePage'])->name('homePage');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/enter',[AuthController::class, 'enter'])->name('enter');
