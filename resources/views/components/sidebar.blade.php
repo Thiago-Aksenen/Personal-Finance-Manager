d05eb6dec3d69d4d0521597f03437f4faf594c7f
Thiago Vinícius Braga Aksenen <aksenen1.thiago@gmail.com>
2025-11-25 09:16:01 -0300

    Icons adicionados

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,11 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
 <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 240px; min-height: 100vh;">
     <ul class="nav nav-pills flex-column mb-auto">
+
         <li class="nav-item">
             <a href="/dashboard" class="nav-link {{ request()->is('dashboard') ? 'active' : 'link-dark' }}">
+                <i class="bi bi-house-door me-2"></i>
                 Dashboard
             </a>
         </li>
@@ -9,6 +13,7 @@
         <li>
             <a href="{{ route('despesa.index') }}"
                 class="nav-link {{ request()->routeIs('despesa.*') ? 'active' : 'link-dark' }}">
+                <i class="bi bi-currency-dollar"></i>
                 Despesas
             </a>
         </li>
@@ -16,6 +21,7 @@
         <li>
             <a href="{{ route('receita.index') }}"
                 class="nav-link {{ request()->routeIs('receita*') ? 'active' : 'link-dark' }}">
+                <i class="bi bi-piggy-bank-fill"></i>
                 Receitas
             </a>
         </li>
